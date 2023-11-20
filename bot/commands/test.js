3cd567a3d04fd349b2a49b52abb421bd9c9888b5
Keith Boone <keithboone1@gmail.com>
2023-11-20 00:03:41 -0500

    Fix hardcoding that accidentally made it to origin

--- bot/commands/test.js
+++ bot/commands/test.js
@@ -4,18 +4,41 @@ import { loadTeam } from '../../database/team.js';
 import { loadAllPlayersOnTeam } from '../../database/player.js';
 import { rightAlign } from './util.js';
 import { postPredictionStandings, updatePrediction, changePredictionsPlayer, postPredictions } from '../features/predictions.js';
+import { loadStandings } from '../../database/standing.js';
 import { setScheduledTime, changeScheduledPlayer, postScheduling } from '../features/schedule.js';
 import { saveDraftSetup } from '../../database/draft.js';
 
 export const TEST_COMMAND = {
-    data: new SlashCommandBuilder()
-        .setName('test')
-        .setDescription('secret command for jumpy to test arbitrary code DO NOT USE')
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+	data: new SlashCommandBuilder()
+		.setName('test')
+		.setDescription('secret command for jumpy to test arbitrary code DO NOT USE')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
 
-    async execute(interaction) {
-        await saveDraftSetup(15, 15, [11, 15, 13, 9, 1, 3, 6, 8, 14, 12, 7, 4]);
+	async execute(interaction) {
+		const standings = await loadStandings(currentSeason.number);
+		await postStandings(1, standings);
+		await postPredictionStandings(currentSeason.number, 1);
 
-        await interaction.reply({ content: 'done!', ephemeral: true });
-    }
+		await interaction.reply({ content: 'done!', ephemeral: true });
+	}
+}
+
+async function postStandings(nextStandingsWeek, standings) {
+  const mainRoom = await channels.fetch(process.env.mainRoomId);
+
+  const standingsText = bold(`Standings at the end of week ${nextStandingsWeek}:\n\n`)
+    .concat(
+      codeBlock(''.concat(
+        ' Rank | Points |  BD  | W | L | T | Team \n',
+        '------|--------|------|---|---|---|------\n',
+        standings.map((standing, index) => prettyTextStanding(index + 1, standing)).join('\n')
+      )
+      )
+    );
+
+  await mainRoom.send(standingsText);
+}
+
+function prettyTextStanding(rank, standing) {
+  return `${rightAlign(6, rank)}|${rightAlign(8, standing.points)}|${rightAlign(6, standing.battle_differential)}|${rightAlign(3, standing.wins)}|${rightAlign(3, standing.losses)}|${rightAlign(3, standing.ties)}| ${standing.teamName}`;
 }
