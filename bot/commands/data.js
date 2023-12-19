77b9ce97c5f9d6b15192920dce8e91fe2eff8ac5
Gabriel Brookman <brookmangabriel@gmail.com>
2023-12-18 22:21:15 -0700

    Add printing team summary tables

--- bot/commands/data.js
+++ bot/commands/data.js
@@ -3,72 +3,179 @@ import { currentSeason } from "../globals.js";
 import { baseFunctionlessHandler } from "./util.js";
 
 import { loadReplays } from "../../database/pairing.js";
+import { loadTeamData } from "../../database/team.js";
 
 export const DATA_COMMAND = {
-    data: new SlashCommandBuilder()
-        .setName('data')
-        .setDescription('Get data from mushi league history')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('scout')
-                .setDescription("Get a player's replay history")
-                .addUserOption(option =>
-                    option
-                        .setName('player')
-                        .setDescription('the player to scout')
-                        .setRequired(true))
-                .addNumberOption(option =>
-                    option
-                        .setName('from_season')
-                        .setDescription('oldest season to get replays from'))
-                .addNumberOption(option =>
-                    option
-                        .setName('through_season')
-                        .setDescription('newest season to get replays from'))),
-
-    async execute(interaction) {
-        switch (interaction.options.getSubcommand()) {
-            case 'scout':
-                await scoutPlayer(interaction);
-                break;
-        }
+  data: new SlashCommandBuilder()
+    .setName("data")
+    .setDescription("Get data from mushi league history")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("scout")
+        .setDescription("Get a player's replay history")
+        .addUserOption((option) =>
+          option
+            .setName("player")
+            .setDescription("the player to scout")
+            .setRequired(true)
+        )
+        .addNumberOption((option) =>
+          option
+            .setName("from_season")
+            .setDescription("oldest season to get replays from")
+        )
+        .addNumberOption((option) =>
+          option
+            .setName("through_season")
+            .setDescription("newest season to get replays from")
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("team")
+        .setDescription("Get a team's player data")
+        .addRoleOption((option) =>
+          option
+            .setName("team")
+            .setDescription("the team to get data for")
+            .setRequired(true)
+        )
+    ),
+  async execute(interaction) {
+    switch (interaction.options.getSubcommand()) {
+      case "scout":
+        await scoutPlayer(interaction);
+        break;
+      case "team":
+        await getTeamData(interaction);
+        break;
     }
+  },
+};
+
+async function getTeamData(interaction) {
+  async function dataCollector(interaction) {
+    const team = interaction.options.getRole("team");
+
+    const playerData = await loadTeamData(team.id);
+
+    return { team, playerData };
+  }
+
+  function verifier(data) {
+    const { team, playerData } = data;
+    let failures = [];
+
+    if (playerData.length === 0) {
+      failures.push(
+        `No data found for ${userMention(team.id)} from the current season.`
+      );
+    }
+
+    return failures;
+  }
+
+  function responseWriter(data) {
+    const { team, playerData } = data;
+
+    function makePlayerIntoRow(p) {
+      return p.name
+        .padEnd(29, " ")
+        .concat("|     ")
+        .concat(p.wins)
+        .concat("|         ")
+        .concat(p.act_wins)
+        .concat("|       ")
+        .concat(p.losses)
+        .concat("|       ")
+        .concat(p.act_losses)
+        .concat("|         ")
+        .concat(p.ties)
+        .concat("|")
+        .concat(p.star_points.toString().padStart(13, " "))
+        .concat("|   ")
+        .concat(Math.ceil(100 * p.stars) / 100);
+    }
+
+    return `Data found for ${userMention(
+      team.id
+    )}:\n\`\`\`Name                         | Wins | Act Wins | Losses | Act Losses | Ties | Star Points | Stars\n`
+      .concat(playerData.map((p) => makePlayerIntoRow(p)).join("\n"))
+      .concat("```");
+  }
+
+  await baseFunctionlessHandler(
+    interaction,
+    dataCollector,
+    verifier,
+    responseWriter,
+    true,
+    true
+  );
 }
 
 async function scoutPlayer(interaction) {
-    async function dataCollector(interaction) {
-        const player = interaction.options.getUser('player');
-        const startSeason = interaction.options.getNumber('from_season') || 0;
-        const endSeason = interaction.options.getNumber('through_season') || currentSeason.number;
+  async function dataCollector(interaction) {
+    const player = interaction.options.getUser("player");
+    const startSeason = interaction.options.getNumber("from_season") || 0;
+    const endSeason =
+      interaction.options.getNumber("through_season") || currentSeason.number;
 
-        const replaysByWeek = await loadReplays(startSeason, endSeason, player.id);
-        const replays = replaysByWeek.flatMap(week => [week.game1, week.game2, week.game3, week.game4, week.game5]).filter(game => !!game);
+    const replaysByWeek = await loadReplays(startSeason, endSeason, player.id);
+    const replays = replaysByWeek
+      .flatMap((week) => [
+        week.game1,
+        week.game2,
+        week.game3,
+        week.game4,
+        week.game5,
+      ])
+      .filter((game) => !!game);
 
-        return { playerSnowflake: player.id, startSeason, endSeason, replays };
+    return { playerSnowflake: player.id, startSeason, endSeason, replays };
+  }
+
+  function verifier(data) {
+    const { playerSnowflake, startSeason, endSeason, replays } = data;
+    let failures = [];
+
+    if (startSeason > endSeason) {
+      failures.push(
+        `You asked for replays ${italic(
+          "after"
+        )} season ${startSeason} but ${italic(
+          "before"
+        )} season ${endSeason}. Obviously there are none.`
+      );
     }
 
-    function verifier(data) {
-        const { playerSnowflake, startSeason, endSeason, replays } = data;
-        let failures = [];
-
-        if (startSeason > endSeason) {
-            failures.push(`You asked for replays ${italic('after')} season ${startSeason} but ${italic('before')} season ${endSeason}. Obviously there are none.`);
-        }
-
-        if (replays.length === 0) {
-            failures.push(`No replays found for ${userMention(playerSnowflake)} between seasons ${startSeason} and ${endSeason}.`)
-        }
-
-        return failures;
+    if (replays.length === 0) {
+      failures.push(
+        `No replays found for ${userMention(
+          playerSnowflake
+        )} between seasons ${startSeason} and ${endSeason}.`
+      );
     }
 
-    function responseWriter(data) {
-        const { playerSnowflake, startSeason, endSeason, replays } = data;
+    return failures;
+  }
 
-        return `Replays found for ${userMention(playerSnowflake)} between seasons ${startSeason} and ${endSeason}:\n`.concat(
-            replays.join('\n')
-        );
-    }
+  function responseWriter(data) {
+    const { playerSnowflake, startSeason, endSeason, replays } = data;
 
-    await baseFunctionlessHandler(interaction, dataCollector, verifier, responseWriter, true, true);
+    return `Replays found for ${userMention(
+      playerSnowflake
+    )} between seasons ${startSeason} and ${endSeason}:\n`.concat(
+      replays.join("\n")
+    );
+  }
+
+  await baseFunctionlessHandler(
+    interaction,
+    dataCollector,
+    verifier,
+    responseWriter,
+    true,
+    true
+  );
 }
--- database/team.js
+++ database/team.js
@@ -1,17 +1,32 @@
-import { db } from './database.js';
+import { db } from "./database.js";
 
 export async function loadTeams() {
-    return await db.all('SELECT id, discord_snowflake FROM team');
+  return await db.all("SELECT id, discord_snowflake FROM team");
 }
 
 export async function loadActiveTeams() {
-    return await db.all('SELECT id, discord_snowflake FROM team WHERE active = 1');
+  return await db.all(
+    "SELECT id, discord_snowflake FROM team WHERE active = 1"
+  );
 }
 
 export async function loadTeam(teamId) {
-    return await db.get('SELECT id, discord_snowflake FROM team WHERE id = ?', teamId);
+  return await db.get(
+    "SELECT id, discord_snowflake FROM team WHERE id = ?",
+    teamId
+  );
 }
 
 export async function loadTeamFromSnowflake(snowflake) {
-    return await db.get('SELECT id, discord_snowflake FROM team WHERE discord_snowflake = ?', snowflake);
+  return await db.get(
+    "SELECT id, discord_snowflake FROM team WHERE discord_snowflake = ?",
+    snowflake
+  );
+}
+
+export async function loadTeamData(snowflake) {
+  return await db.all(
+    "SELECT player.name, wins, act_wins, losses, act_losses, ties, star_points, pstat.stars FROM pstat INNER JOIN player ON player.id = pstat.player INNER JOIN team ON player.team = team.id WHERE team.discord_snowflake = ? AND season = 15 ORDER BY -pstat.stars;",
+    snowflake // KEITH HELP I HARDCODED SEASON = 15
+  );
 }
