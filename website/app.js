085d9a2acfd7750d04ef19cb0592c5dbc0808817
Gabriel Brookman <brookmangabriel@gmail.com>
2024-06-24 17:16:57 -0600

    keith comments on app.js

--- website/app.js
+++ website/app.js
@@ -9,7 +9,7 @@ import { loadCurrentSeason } from "../database/season.js";
 import { openDb } from "../database/database.js";
 
 const app = express();
-const port = 3030;
+const port = 3000;
 
 app.get("/teamlist", async (req, res) => {
   const x = await loadActiveTeams();
@@ -18,19 +18,15 @@ app.get("/teamlist", async (req, res) => {
 });
 
 app.get("/teaminfo/:id", async (req, res) => {
-  console.log("hullo");
-  console.log((await loadCurrentSeason()).number);
   const result = await loadTeamSheet(
     req.params.id,
     (await loadCurrentSeason()).number
   );
-  console.log(result);
 
   res.set("Access-Control-Allow-Origin", "*");
   res.send(JSON.stringify(result));
 });
 
 app.listen(port, async () => {
-  console.log(`Example app listening on port ${port}!`);
   await openDb();
 });
